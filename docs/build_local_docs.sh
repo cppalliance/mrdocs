5a559f733ba3e6853078aa592a348fb4b7259357
alandefreitas <alandefreitas@gmail.com>
2023-11-02 20:05:04 -0300

    docs: antora playbooks refer to mrdocs repo
    The previously had to still refer to the mrdox repository.

---
+++ docs/build_local_docs.sh
@@ -0,0 +1,12 @@
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Copyright (c) 2023 Alan de Freitas (alandefreitas@gmail.com)
+#
+# Official repository: https://github.com/cppalliance/mrdocs
+#
+
+echo "Building local documentation with Antora..."
+./build_docs.sh local-antora-playbook.yml
