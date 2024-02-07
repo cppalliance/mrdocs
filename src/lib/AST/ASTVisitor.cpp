8c602e6e155c4f86745dbaab7c800e8866b8572e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-02-07 20:11:11 +0100

    do not use CXXMethodDecl::isExplicitObjectMem

--- src/lib/AST/ASTVisitor.cpp
+++ src/lib/AST/ASTVisitor.cpp
@@ -1800,7 +1800,6 @@ public:
                 convertToReferenceKind(
                     D->getRefQualifier());
             I.specs0.isFinal |= D->template hasAttr<FinalAttr>();
-            I.specs1.isExplicitObjectMemberFunction |= D->isExplicitObjectMemberFunction();
             //D->isCopyAssignmentOperator()
             //D->isMoveAssignmentOperator()
             //D->isOverloadedOperator();
