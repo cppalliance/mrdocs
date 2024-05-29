f45b47bd1b954da163f5f4c6409e03038991024a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-29 18:05:15 -0300

    fix: HTML template generator alignment and CI issues

--- src/lib/Gen/html/Builder.cpp
+++ src/lib/Gen/html/Builder.cpp
@@ -42,22 +42,22 @@ Builder(
     std::string partialsPath = files::appendPath(
         config->addonsDir, "generator", "html", "partials");
     forEachFile(partialsPath, true,
-                [&](std::string_view pathName) -> Error
-    {
-        fs::path path = pathName;
-        if(path.extension() != ".hbs")
+        [&](std::string_view pathName) -> Error
+        {
+            fs::path path = pathName;
+            if(path.extension() != ".hbs")
+                return Error::success();
+            path = path.lexically_relative(partialsPath);
+            while(path.has_extension())
+                path.replace_extension();
+
+            auto text = files::getFileText(pathName);
+            if (! text)
+                return text.error();
+
+            hbs_.registerPartial(
+                path.generic_string(), *text);
             return Error::success();
-        path = path.lexically_relative(partialsPath);
-        while(path.has_extension())
-            path.replace_extension();
-
-        auto text = files::getFileText(pathName);
-        if (! text)
-            return text.error();
-
-        hbs_.registerPartial(
-            path.generic_string(), *text);
-        return Error::success();
     }).maybeThrow();
 
     // Load JavaScript helpers
@@ -68,8 +68,7 @@ Builder(
         {
             // Register JS helper function in the global object
             constexpr std::string_view ext = ".js";
-            if (!pathName.ends_with(ext))
-                return {};
+            if (!pathName.ends_with(ext)) return {};
             auto name = files::getFileName(pathName);
             name.remove_suffix(ext.size());
             MRDOCS_TRY(auto script, files::getFileText(pathName));
@@ -82,7 +81,47 @@ Builder(
         dom::makeInvocable([res = config->multiPage]() -> Expected<dom::Value> {
         return res;
     }));
+
+    hbs_.registerHelper("primary_location",
+        dom::makeInvocable([](dom::Value const& v) ->
+            dom::Value
+        {
+            dom::Value src_loc = v.get("loc");
+            if(! src_loc)
+                return nullptr;
+            dom::Value decls = src_loc.get("decl");
+            if(dom::Value def = src_loc.get("def"))
+            {
+                // for classes/enums, prefer the definition
+                dom::Value kind = v.get("kind");
+                if(kind == "record" || kind == "enum")
+                    return def;
+
+                // we only every want to use the definition
+                // for non-tag types when no other declaration
+                // exists
+                if(! decls)
+                    return def;
+            }
+            if(! decls.isArray())
+                return nullptr;
+            dom::Value first;
+            // otherwise, use whatever declaration had docs.
+            // if no declaration had docs, fallback to the
+            // first declaration
+            for(const dom::Value& loc : decls.getArray())
+            {
+                if(loc.get("documented"))
+                    return loc;
+                else if(! first)
+                    first = loc;
+            }
+            return first;
+        }));
+
+    helpers::registerStringHelpers(hbs_);
     helpers::registerAntoraHelpers(hbs_);
+    helpers::registerContainerHelpers(hbs_);
 }
 
 //------------------------------------------------
@@ -105,7 +144,6 @@ callTemplate(
     MRDOCS_TRY(auto fileText, files::getFileText(pathName));
     HandlebarsOptions options;
     options.noEscape = true;
-
     Expected<std::string, HandlebarsError> exp =
         hbs_.try_render(fileText, context, options);
     if (!exp)
@@ -131,6 +169,20 @@ renderSinglePageFooter()
 
 //------------------------------------------------
 
+std::string
+Builder::
+getRelPrefix(std::size_t depth)
+{
+    std::string rel_prefix;
+    if(! depth ||! domCorpus_->config->multiPage)
+        return rel_prefix;
+    --depth;
+    rel_prefix.reserve(depth * 3);
+    while(depth--)
+        rel_prefix.append("../");
+    return rel_prefix;
+}
+
 dom::Value
 Builder::
 createContext(
@@ -141,6 +193,20 @@ createContext(
         });
 }
 
+dom::Value
+Builder::
+createContext(
+    OverloadSet const& OS)
+{
+    dom::Object::storage_type props;
+    props.emplace_back("symbol",
+        domCorpus_.getOverloads(OS));
+    const Info& Parent = domCorpus_->get(OS.Parent);
+    props.emplace_back("relfileprefix",
+        getRelPrefix(Parent.Namespace.size() + 1));
+    return dom::Object(std::move(props));
+}
+
 template<class T>
 Expected<std::string>
 Builder::
@@ -149,6 +215,15 @@ operator()(T const& I)
     return callTemplate(
         "single-symbol.html.hbs",
         createContext(I.id));
+}
+
+Expected<std::string>
+Builder::
+operator()(OverloadSet const& OS)
+{
+    return callTemplate(
+        "overload-set.html.hbs",
+        createContext(OS));
 }
 
 #define DEFINE(T) template Expected<std::string> \
