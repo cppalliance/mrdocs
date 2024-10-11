9fcbb5acebec06d5d865b89c36f657f83774b8c3
Nerixyz <nerixdev@outlook.de>
2024-10-11 22:44:36 +0200

    feat: generate YAML schema for config

---
+++ util/generate-yaml-schema.py
@@ -0,0 +1,142 @@
+import json
+import sys
+import os
+
+# NotRequired was added in 3.11
+if sys.version_info < (3, 11):
+    from typing import Dict, Any
+
+    Option = Dict[str, Any]
+    OptionGroup = Dict[str, Any]
+    SchemaType = Dict[str, Any]
+else:
+    from typing import TypedDict, NotRequired, Any
+
+    Option = TypedDict(
+        "Option",
+        {
+            "name": str,
+            "command-line-only": NotRequired[bool],
+            "brief": str,
+            "details": NotRequired[str],
+            "type": NotRequired[str],  # if absent, it's an object
+            "default": NotRequired[str | int | list[int]],
+            "required": NotRequired[bool],
+            "values": NotRequired[list[str]],
+            "options": NotRequired[list["Option"]],
+            "min-value": NotRequired[int | float],
+            "max-value": NotRequired[int | float],
+        },
+    )
+    OptionGroup = TypedDict(
+        "OptionGroup",
+        {
+            "category": str,
+            "command-line-only": NotRequired[bool],
+            "brief": str,
+            "details": NotRequired[str],
+            "options": list[Option],
+        },
+    )
+    SchemaType = dict[str, Any]
+
+
+def to_yaml_schema_type(option: Option) -> SchemaType:
+    has_suboptions = "options" in option
+    if has_suboptions or "type" not in option:
+        return {"type": "object"}
+
+    option_type = option["type"]
+    if option_type == "bool":
+        return {"type": "boolean"}
+    if option_type == "int":
+        return {"type": "integer"}
+    if option_type == "unsigned":
+        return {"type": "integer", "minimum": 0}
+    if option_type in ["string", "file-path", "dir-path", "path"]:
+        return {"type": "string"}
+    if option_type == "enum":
+        assert "values" in option
+        return {"enum": option["values"]}
+    if option_type in [
+        "list<string>",
+        "list<path>",
+        "list<file-path>",
+        "list<dir-path>",
+    ]:
+        return {"type": "array", "items": {"type": "string"}}
+    raise ValueError(
+        f"to_yaml_schema_type: Cannot convert option type {option_type} to JSON/YAML schema type"
+    )
+
+
+def yaml_schema_required_props(options: list[Option]):
+    return [opt["name"] for opt in options if "required" in opt and opt["required"]]
+
+
+def to_yaml_schema_default_value(option: Option):
+    has_suboptions = "options" in option
+    if has_suboptions or "default" not in option:
+        return None
+
+    return option["default"]
+
+
+def yaml_schema_property(option: Option):
+    prop = to_yaml_schema_type(option)
+    if "min-value" in option:
+        prop["minimum"] = option["min-value"]
+    if "max-value" in option:
+        prop["maximum"] = option["max-value"]
+    default_value = to_yaml_schema_default_value(option)
+    if default_value is not None:
+        prop["default"] = default_value
+    prop["title"] = option["brief"]
+    if "details" in option:
+        prop["description"] = option["details"]
+    if "options" in option:
+        prop["properties"] = yaml_schema_properties(option["options"])
+        prop["required"] = yaml_schema_required_props(option["options"])
+    return prop
+
+
+def yaml_schema_properties(options: list[Option]):
+    props = {}
+    for option in options:
+        props[option["name"]] = yaml_schema_property(option)
+    return props
+
+
+def generate_yaml_schema(config: list[OptionGroup]):
+    root = {
+        # draft-07 is the newest supported one and we don't require any other features
+        "$schema": "http://json-schema.org/draft-07/schema",
+        "title": "MrDocs Configuration",
+        "type": "object",
+        "properties": {},
+        "required": [],
+    }
+
+    for category in config:
+        opts = [
+            opt
+            for opt in category["options"]
+            if "command-line-only" not in opt or not opt["command-line-only"]
+        ]
+        root["properties"] |= yaml_schema_properties(opts)
+        root["required"] += yaml_schema_required_props(opts)
+
+    return json.dumps(root, indent=2, sort_keys=True)
+
+if __name__ == "__main__":
+    mrdocs_root_dir = os.path.join(os.path.dirname(__file__), '..')
+    mrdocs_schema_path = os.path.join(mrdocs_root_dir, 'docs', 'mrdocs.schema.json')
+    mrdocs_config_path = os.path.join(mrdocs_root_dir, 'src', 'lib', 'Lib', 'ConfigOptions.json')
+
+    with open(mrdocs_config_path, 'r') as f:
+        config = json.loads(f.read())
+    
+    yaml_schema = generate_yaml_schema(config)
+
+    with open(mrdocs_schema_path, 'w') as f:
+        f.write(yaml_schema)
