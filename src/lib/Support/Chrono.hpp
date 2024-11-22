7dfcb8e3f83442772b2ccb53572884d29a4031b0
alandefreitas <alandefreitas@gmail.com>
2024-11-22 13:12:37 -0300

    refactor: time report supports minutes

--- src/lib/Support/Chrono.hpp
+++ src/lib/Support/Chrono.hpp
@@ -20,22 +20,46 @@
 namespace clang {
 namespace mrdocs {
 
+/** Formats a duration into a human-readable string.
+
+    This function takes a `std::chrono::duration` object and formats it into a
+    string that represents the duration in a human-readable format. The format
+    includes hours, minutes, seconds, and milliseconds as appropriate.
+
+    @param delta The duration to format.
+    @return A string representing the formatted duration.
+ */
 template <class Rep, class Period>
 std::string
 format_duration(
     std::chrono::duration<Rep, Period> delta)
 {
-    auto delta_ms = std::chrono::duration_cast<
-                        std::chrono::milliseconds>(delta).count();
-    if (delta_ms < 1000)
+    auto delta_ms = std::chrono::duration_cast<std::chrono::milliseconds>(delta);
+    if (delta < std::chrono::seconds(1))
     {
-        return fmt::format("{} ms", delta_ms);
+        return fmt::format("{}ms", delta_ms.count());
     }
-    else
+
+    auto delta_s = std::chrono::duration_cast<std::chrono::seconds>(delta);
+    if (delta < std::chrono::minutes(1))
     {
-        double const delta_s = static_cast<double>(delta_ms) / 1000.0;
-        return fmt::format("{:.02f} s", delta_s);
+        delta_ms -= delta_s;
+        return fmt::format("{}s {}ms", delta_s.count(), delta_ms.count());
     }
+
+    auto delta_min = std::chrono::duration_cast<std::chrono::minutes>(delta);
+    if (delta < std::chrono::hours(1))
+    {
+        delta_ms -= delta_s;
+        delta_s -= delta_min;
+        return fmt::format("{}min {}s {}ms", delta_min.count(), delta_s.count(), delta_ms.count());
+    }
+
+    auto delta_h = std::chrono::duration_cast<std::chrono::hours>(delta);
+    delta_ms -= delta_s;
+    delta_s -= delta_min;
+    delta_min -= delta_h;
+    return fmt::format("{}h {}min {}s {}ms", delta_h.count(), delta_min.count(), delta_s.count(), delta_ms.count());
 }
 
 } // mrdocs
