171e0f2a6456a39c570972a4ef8f84be927a2100
alandefreitas <alandefreitas@gmail.com>
2025-01-06 22:41:05 -0300

    javadoc ref can resolve to alternative URLs
    #feat

--- src/lib/Gen/hbs/VisitorHelpers.hpp
+++ src/lib/Gen/hbs/VisitorHelpers.hpp
@@ -24,6 +24,24 @@ MRDOCS_DECL
 bool
 shouldGenerate(Info const& I);
 
+/** Find an Info type whose URL we can use for the specified Info
+
+    When we should not generate a page for the Info as per
+    @ref shouldGenerate, other documentation pages might
+    still link to it.
+
+    In this case, we find a related Info type whose URL
+    we can use for the specified Info.
+
+    For specializations, we typically look for their primary
+    template. For record and enum members, we look for
+    the parent record or enum. For other Info types, we
+    return nullptr.
+ */
+MRDOCS_DECL
+Info const*
+findAlternativeURLInfo(Corpus const& c, Info const& I);
+
 } // clang::mrdocs::hbs
 
 #endif
