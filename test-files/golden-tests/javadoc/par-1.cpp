16ea2bd8374fffb437e99b72991181abb8bffd73
alandefreitas <alandefreitas@gmail.com>
2025-01-13 12:22:36 -0300

    render orphan @par headings
    Render @par headings when the @par has no children paragraphs
    
    #feat
    
    fix #787

---
+++ test-files/golden-tests/javadoc/par-1.cpp
@@ -0,0 +1,43 @@
+/** Brief
+
+    @par Custom par
+    Paragraph 1
+
+    @code
+    void f1();
+    @endcode
+
+ */
+void f1();
+
+/** Brief
+
+    @par Custom par
+
+    Paragraph 2
+
+    @code
+    void f2();
+    @endcode
+
+ */
+void f2();
+
+/** Brief
+
+    @par Custom par
+    @code
+    void f3();
+    @endcode
+ */
+void f3();
+
+/** Brief
+
+    @par Custom par
+
+    @code
+    void f4();
+    @endcode
+ */
+void f4();
