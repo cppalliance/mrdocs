d235a690169e18a5792206bdcdd955da5fb2ee50
alandefreitas <alandefreitas@gmail.com>
2025-02-26 16:34:26 -0300

    refs match fundamental type variants
    #feat

---
+++ test-files/golden-tests/javadoc/copydoc/fundamental.cpp
@@ -0,0 +1,19 @@
+/** Fail
+
+    @param a Fail param
+ */
+void f(int a);
+
+/** Brief
+
+    Details
+
+    @param a Param description
+ */
+void f(int long a);
+
+/// @copydoc f(long int)
+void g(long int a);
+
+/// @copydoc f(long)
+void h(long a);
