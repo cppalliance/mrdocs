2dcfe6839c5abcc11e3cc3ebb0c49949907cc3a8
Alan de Freitas <alandefreitas@gmail.com>
2025-08-29 23:45:50 -0500

    fix: using decl shadows do not contain excluded symbols

--- test-files/golden-tests/symbols/using/using-function-excluded.cpp
+++ test-files/golden-tests/symbols/using/using-function-excluded.cpp
@@ -1,7 +1,14 @@
 namespace A
 {
+    // This symbol is excluded.
     void f(int);
 }
 
+/** Using excluded function f
+
+    No shadow should be listed because A::f is excluded.
+
+    Only included symbols are listed in the shadow list.
+ */
 using A::f;
 
