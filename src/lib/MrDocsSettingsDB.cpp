9afededbfe293f2e47fa2d7266b80772b0d0cb04
Alan de Freitas <alandefreitas@gmail.com>
2025-09-12 03:24:41 -0500

    feat: MrDocsSettings compilation database
    Generate compile commands according to the glob patterns defined in the configuration file

---
+++ src/lib/MrDocsSettingsDB.cpp
@@ -0,0 +1,115 @@
+//
+// This is a derivative work. originally part of the LLVM Project.
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (c) 2023 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Official repository: https://github.com/cppalliance/mrdocs
+//
+
+#include "MrDocsSettingsDB.hpp"
+#include <mrdocs/Support/Path.hpp>
+
+namespace clang {
+namespace mrdocs {
+
+MrDocsSettingsDB::MrDocsSettingsDB(ConfigImpl const& config)
+{
+    std::vector<std::string> sourceFiles;
+    auto& s = config.settings();
+    for (auto const& curInput: s.input)
+    {
+        forEachFile(
+            curInput,
+            s.recursive,
+            [&](std::string_view path) -> Expected<void> {
+            if (files::isDirectory(path))
+            {
+                return {};
+            }
+            // Check file patterns that should match
+            auto inputFilename = files::getFileName(path);
+            if (std::ranges::none_of(
+                    s.filePatterns,
+                    [&](PathGlobPattern const& pattern) {
+                return pattern.match(inputFilename);
+            }))
+            {
+                return {};
+            }
+            // Check file or directories in path that should be skipped
+            if (std::ranges::any_of(s.exclude, [&](std::string const& excludePath) {
+                return files::startsWith(path, excludePath);
+            }))
+            {
+                return {};
+            }
+            // Check if the relative path matches any of the s.excludePatterns
+            if (std::ranges::any_of(
+                    s.excludePatterns,
+                    [&](PathGlobPattern const& pattern) {
+                return pattern.match(path);
+            }))
+            {
+                return {};
+            }
+            sourceFiles.emplace_back(path);
+            return {};
+        });
+    }
+
+    for (auto const& pathName: sourceFiles)
+    {
+        // auto fileName = files::getFileName(pathName);
+        auto parentDir = files::getParentDir(pathName);
+
+        std::vector<std::string> cmds;
+        cmds.emplace_back("clang");
+        cmds.emplace_back("-fsyntax-only");
+        cmds.emplace_back("-std=c++23");
+        cmds.emplace_back("-pedantic-errors");
+        cmds.emplace_back("-Werror");
+        cmds.emplace_back("-x");
+        cmds.emplace_back("c++");
+        cmds.emplace_back(pathName);
+        cc_.emplace_back(parentDir, pathName, std::move(cmds), parentDir);
+        cc_.back().Heuristic = "generated from mrdocs.yml";
+    }
+}
+
+std::vector<tooling::CompileCommand>
+MrDocsSettingsDB::getCompileCommands(llvm::StringRef FilePath) const
+{
+    std::vector<tooling::CompileCommand> result;
+    for (auto const& cmd: cc_)
+    {
+        if (cmd.Filename == FilePath)
+        {
+            result.push_back(cmd);
+        }
+    }
+    return result;
+}
+
+std::vector<std::string>
+MrDocsSettingsDB::getAllFiles() const
+{
+    std::vector<std::string> result;
+    result.reserve(cc_.size());
+    for (auto const& cmd: cc_)
+    {
+        result.push_back(cmd.Filename);
+    }
+    return result;
+}
+
+std::vector<tooling::CompileCommand>
+MrDocsSettingsDB::getAllCompileCommands() const
+{
+    return cc_;
+}
+
+} // namespace mrdocs
+} // namespace clang
--- src/tool/GenerateAction.cpp
+++ src/tool/GenerateAction.cpp
@@ -10,80 +10,18 @@
 // Official repository: https://github.com/cppalliance/mrdocs
 //
 
-#include "CompilerInfo.hpp"
 #include "ToolArgs.hpp"
-#include "lib/Support/CMakeExecution.hpp"
+#include "ToolCompilationDatabase.hpp"
 #include "lib/ConfigImpl.hpp"
 #include "lib/CorpusImpl.hpp"
 #include "lib/MrDocsCompilationDatabase.hpp"
 #include "lib/Support/Path.hpp"
-#include "llvm/Support/Program.h"
 #include <mrdocs/Generators.hpp>
 #include <mrdocs/Support/Report.hpp>
 #include <mrdocs/Support/Path.hpp>
 #include <mrdocs/Support/ThreadPool.hpp>
-#include <clang/Tooling/JSONCompilationDatabase.h>
-
-#include <cstdlib>
 
 namespace clang::mrdocs {
-
-namespace {
-
-/**
- * Conditionally generates a `compile_commands.json` file based on the provided project path.
- *
- * This function evaluates the project path to decide the appropriate action regarding the generation of a `compile_commands.json` file:
- * 1. If the project path is a `compile_commands.json` file, it returns the path as-is, with no database generation.
- * 2. If the project path is a directory, it generates the compilation database using the provided project path.
- * 3. If the project path is a `CMakeLists.txt` file, it generates the compilation database using the parent directory of the file.
- *
- * @param inputPath The path to the project, which can be a directory, a `compile_commands.json` file, or a `CMakeLists.txt` file.
- * @param cmakeArgs The arguments to pass to CMake when generating the compilation database.
- * @return An `Expected` object containing the path to the `compile_commands.json` file if the database is generated, or the provided path if it is already the `compile_commands.json` file. 
- * Returns an `Unexpected` object in case of failure (e.g., file not found, CMake execution failure).
- */
-Expected<std::string>
-generateCompileCommandsFile(llvm::StringRef inputPath, llvm::StringRef cmakeArgs, llvm::StringRef buildDir)
-{
-    namespace fs = llvm::sys::fs;
-    namespace path = llvm::sys::path;
-
-    fs::file_status fileStatus;
-    MRDOCS_CHECK(files::exists(inputPath), formatError("File does not exist: '{}'", inputPath.operator std::string_view()));
-    MRDOCS_CHECK(!fs::status(inputPath, fileStatus), "Failed to get file status");
-
-    // --------------------------------------------------------------
-    // Input path is a project directory
-    // --------------------------------------------------------------
-    if (fs::is_directory(fileStatus))
-    {
-        return executeCmakeExportCompileCommands(inputPath, cmakeArgs, buildDir);
-    }
-
-    // --------------------------------------------------------------
-    // Input path is a CMakeLists.txt
-    // --------------------------------------------------------------
-    auto const fileName = files::getFileName(inputPath);
-    if (fileName == "CMakeLists.txt")
-    {
-        std::string cmakeSourceDir = files::getParentDir(inputPath);
-        return executeCmakeExportCompileCommands(
-            cmakeSourceDir, cmakeArgs, buildDir);
-    }
-
-    // --------------------------------------------------------------
-    // Input path is a compile_commands.json
-    // --------------------------------------------------------------
-    if (fileName == "compile_commands.json")
-    {
-        return inputPath.str();
-    }
-
-    return Unexpected(Error("Input path is not a directory, a CMakeLists.txt file, or a compile_commands.json file"));
-}
-
-} // anonymous namespace
 
 
 Expected<void>
@@ -122,21 +60,9 @@ DoGenerateAction(
 
     // --------------------------------------------------------------
     //
-    // Find or generate the compile_commands.json
+    // Find or generate the compilation database
     //
     // --------------------------------------------------------------
-    std::string compilationDatabasePath = settings.compilationDatabase;
-    if (compilationDatabasePath.empty())
-    {
-        std::string c = files::appendPath(settings.sourceRoot, "CMakeLists.txt");
-        if (files::exists(c))
-        {
-            compilationDatabasePath = c;
-        }
-    }
-    MRDOCS_CHECK(
-        compilationDatabasePath,
-        "The compilation database path argument is missing");
     ScopedTempDirectory tempDir(
         config->settings().outputDir(),
         ".temp");
@@ -145,48 +71,9 @@ DoGenerateAction(
         report::error("Failed to create temporary directory: {}", tempDir.error());
         return Unexpected(tempDir.error());
     }
-    std::string buildPath = files::appendPath(tempDir, "build");
-    Expected<std::string> const compileCommandsPathExp =
-        generateCompileCommandsFile(
-            compilationDatabasePath, settings.cmake, buildPath);
-    if (!compileCommandsPathExp)
-    {
-        report::error(
-            "Failed to generate compile_commands.json file: {}",
-            compileCommandsPathExp.error());
-        return Unexpected(compileCommandsPathExp.error());
-    }
-
-    // --------------------------------------------------------------
-    //
-    // Load the compilation database file
-    //
-    // --------------------------------------------------------------
-    std::string compileCommandsPath = files::normalizePath(*compileCommandsPathExp);
-    MRDOCS_TRY(compileCommandsPath, files::makeAbsolute(compileCommandsPath));
-    std::string errorMessage;
-    std::unique_ptr<clang::tooling::JSONCompilationDatabase>
-        jsonDatabasePtr =
-        tooling::JSONCompilationDatabase::loadFromFile(
-            compileCommandsPath,
-            errorMessage,
-            tooling::JSONCommandLineSyntax::AutoDetect);
-    if (!jsonDatabasePtr)
-    {
-        return Unexpected(formatError(
-            "Failed to load compilation database: {}", errorMessage));
-    }
-    clang::tooling::JSONCompilationDatabase& jsonDatabase = *jsonDatabasePtr;
-
-    // Custom compilation database that applies settings from the configuration
-    auto const defaultIncludePaths = getCompilersDefaultIncludeDir(
-        jsonDatabase, (*config)->useSystemStdlib);
-    auto compileCommandsDir = files::getParentDir(compileCommandsPath);
-    MrDocsCompilationDatabase compilationDatabase(
-        compileCommandsDir,
-        jsonDatabase,
-        config,
-        defaultIncludePaths);
+    MRDOCS_TRY(
+        MrDocsCompilationDatabase compilationDatabase,
+        generateCompilationDatabase(tempDir.path(), config, threadPool));
 
     // --------------------------------------------------------------
     //
