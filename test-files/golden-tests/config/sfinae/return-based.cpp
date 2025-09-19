39c7a5682b7ba4b2add278c39a2dff457946cc6d
Agustin Berge <agustinberge@gmail.com>
2025-09-19 16:14:19 +0200

    std

--- test-files/golden-tests/config/sfinae/return-based.cpp
+++ test-files/golden-tests/config/sfinae/return-based.cpp
@@ -1,6 +1,28 @@
 // issue #849
 
-#include <type_traits>
+namespace std
+{
+template <bool C, typename T = void>
+struct enable_if
+{
+    using type = T;
+};
+template <typename T>
+struct enable_if<false, typename T>
+{};
+
+template <bool C, typename T = void>
+using enable_if_t = typename enable_if<C, T>::type;
+
+template <typename T>
+struct is_class
+{
+    static constexpr bool value = true;
+};
+
+template <typename T>
+bool is_class_v = is_class<T>::value;
+}
 
 template <typename T>
 std::enable_if_t<std::is_class_v<T>, int> f();
