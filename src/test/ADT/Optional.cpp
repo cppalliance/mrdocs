e77989ef74787ca5d59b11456796588ef6d784f1
Alan de Freitas <alandefreitas@gmail.com>
2025-09-25 22:40:27 -0500

    feat(lib): optional references
    Support Optional for references and replace usages of Optional with reference wrappers in the codebase.

--- src/test/ADT/Optional.cpp
+++ src/test/ADT/Optional.cpp
@@ -11,6 +11,8 @@
 #include <mrdocs/ADT/Optional.hpp>
 #include <mrdocs/Metadata/Info/Source.hpp>
 #include <test_suite/test_suite.hpp>
+#include <optional>
+#include <type_traits>
 
 namespace clang::mrdocs {
 
@@ -217,6 +219,110 @@ struct OptionalTest {
     }
 
     void
+    test_reference_optional()
+    {
+        static_assert(detail::isOptionalV<Optional<int&>>);
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        Optional<int&> r = a;
+        BOOST_TEST(r.has_value());
+        BOOST_TEST(&*r == &a);
+        BOOST_TEST(r.value() == 1);
+
+        struct S {
+            int v{ 0 };
+
+            int
+            inc()
+            {
+                return ++v;
+            }
+        };
+        S s{ 7 };
+        Optional<S&> rs = s;
+        BOOST_TEST(rs->inc() == 8);
+        BOOST_TEST((*rs).v == 8);
+
+        r = b;
+        BOOST_TEST(&*r == &b);
+        BOOST_TEST(*r == 2);
+
+        // assign from Optional<int&>
+        Optional<int&> rr = a;
+        rr = r;
+        BOOST_TEST(&*rr == &b);
+
+        // construct from Optional<int&>
+        Optional<int&> rr2{ r };
+        BOOST_TEST(&*rr2 == &b);
+
+        // Rebinding from Optional<int>
+        Optional<int> vo = 42;
+        rr = vo; // rebind to the referent inside vo
+        BOOST_TEST(&*rr == &*vo);
+        BOOST_TEST(*rr == *vo);
+
+        // rebind to different lvalue
+        rr2 = c;
+        BOOST_TEST(&*rr2 == &c);
+        BOOST_TEST(*rr2 == 3);
+
+        rr2.reset();
+        BOOST_TEST(!rr2.has_value());
+        rr2 = a;
+        BOOST_TEST(&*rr2 == &a);
+
+        Optional<int&> copy = rr2;
+        BOOST_TEST(&*copy == &a);
+        Optional<int&> moved = std::move(rr2);
+        BOOST_TEST(&*moved == &a);
+
+        Optional<int&> rx = b;
+        Optional<int&> ry = c;
+        rx.swap(ry);
+        BOOST_TEST(&*rx == &c);
+        BOOST_TEST(&*ry == &b);
+
+        Optional<int&> r1 = a;
+        Optional<int&> r2 = b;
+        BOOST_TEST(r1 != r2);
+        BOOST_TEST((r1 < r2) == (a < b));
+
+        Optional<int&> rn;
+        BOOST_TEST(rn == std::nullopt);
+        BOOST_TEST((r1 <=> std::nullopt) == std::strong_ordering::greater);
+        BOOST_TEST((rn <=> std::nullopt) == std::strong_ordering::equal);
+
+        int z = 9;
+        Optional<int&> rz = z;
+        int& zref = std::move(rz).value();
+        BOOST_TEST(&zref == &z);
+
+        static_assert(!std::is_constructible_v<Optional<int&>, int&&>);
+        static_assert(!std::is_constructible_v<Optional<int&>, int const&&>);
+        static_assert(!std::is_constructible_v<Optional<int&>, Optional<int>&&>);
+        static_assert(!std::is_constructible_v<Optional<int&>, Optional<int> const&&>);
+        static_assert(!std::is_assignable_v<Optional<int&>, int&&>);
+        static_assert(!std::is_assignable_v<Optional<int&>, int const&&>);
+
+        static_assert(std::is_constructible_v<Optional<int&>, int&>);
+        static_assert(std::is_assignable_v<Optional<int&>, int&>);
+        static_assert(std::is_constructible_v<Optional<int&>, Optional<int&>&>);
+        static_assert(std::is_constructible_v<Optional<int&>, Optional<int&> const&>);
+        static_assert(std::is_assignable_v<Optional<int&>, Optional<int&>&>);
+        static_assert(std::is_assignable_v<Optional<int&>, Optional<int&> const&>);
+
+        Optional<int> on = 1;
+        Optional<int&> orf = a;
+        BOOST_TEST(orf == on);
+        a = 5;
+        BOOST_TEST(orf != on);
+    }
+
+    void
     run()
     {
         test_nullable_string();
@@ -225,6 +331,7 @@ struct OptionalTest {
         test_nullable_enum();
         test_nullable_location();
         test_fallback_notraits();
+        test_reference_optional();
     }
 };
 
