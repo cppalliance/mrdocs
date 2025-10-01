2346709682645f3ea7094152a6c924fb68a038fc
Agustin Berge <agustinberge@gmail.com>
2025-10-01 12:54:12 +0200

    salvage some tests...

---
+++ src/test/lib/MrDocsCompilationDatabase.cpp
@@ -0,0 +1,135 @@
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (c) 2023 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Official repository: https://github.com/cppalliance/mrdocs
+//
+
+#include "lib/ConfigImpl.hpp"
+#include "lib/MrDocsCompilationDatabase.hpp"
+#include "lib/SingleFileDB.hpp"
+#include <mrdocs/Support/Algorithm.hpp>
+#include <mrdocs/Support/Path.hpp>
+#include <mrdocs/Support/ThreadPool.hpp>
+#include <test_suite/test_suite.hpp>
+
+namespace clang {
+namespace mrdocs {
+
+namespace {
+struct TestConfigImpl final : Config
+{
+    Settings settings_;
+
+    ThreadPool&
+    threadPool() const noexcept override
+    {
+        static ThreadPool dummyThreadPool_;
+        return dummyThreadPool_;
+    }
+
+    dom::Object const& object() const override
+    {
+        static dom::Object dummyObject;
+        return dummyObject;
+
+    }
+
+    Settings const& settings() const noexcept override
+    {
+        return settings_;
+    }
+};
+}
+
+struct MrDocsCompilationDatabase_test
+{
+    Config::Settings fileSettings_;
+    ThreadPool threadPool_;
+    ReferenceDirectories dirs_;
+
+    auto adjustCompileCommand(SingleFileDB const& db, std::shared_ptr<Config const> config) const
+    {
+        // Create an adjusted MrDocsDatabase
+        std::unordered_map<std::string, std::vector<std::string>> defaultIncludePaths;
+        MrDocsCompilationDatabase compilations(
+            dirs_.cwd, db, config, defaultIncludePaths);
+        return compilations.getAllCompileCommands().front().CommandLine;
+    }
+
+    static bool has(std::vector<std::string> const& commandLine, std::string_view flag)
+    {
+        return mrdocs::contains(commandLine, flag);
+    }
+    static bool has(std::vector<std::string> const& commandLine, std::initializer_list<std::string_view> flags)
+    {
+        MRDOCS_ASSERT(flags.size() > 0);
+        return std::search(commandLine.begin(), commandLine.end(), flags.begin(), flags.end()) != commandLine.end();
+    }
+
+    void testClang()
+    {
+        SingleFileDB const db = SingleFileDB::makeForClang("test.cpp");
+
+        {
+            std::shared_ptr<TestConfigImpl> config = std::make_shared<TestConfigImpl>();
+            config->settings_.useSystemStdlib = false;
+            config->settings_.stdlibIncludes.push_back("stdlib-path");
+
+            auto adjusted = adjustCompileCommand(db, config);
+            BOOST_TEST(has(adjusted, "-nostdinc++"));
+            BOOST_TEST(has(adjusted, {"-isystem", "stdlib-path"}));
+        }
+
+        {
+            std::shared_ptr<TestConfigImpl> config = std::make_shared<TestConfigImpl>();
+            config->settings_.useSystemLibc = false;
+            config->settings_.libcIncludes.push_back("libc-path");
+
+            auto adjusted = adjustCompileCommand(db, config);
+            BOOST_TEST(has(adjusted, "-nostdinc"));
+            BOOST_TEST(has(adjusted, {"-isystem", "libc-path"}));
+        }
+    }
+
+    void testClangCL()
+    {
+        SingleFileDB const db = SingleFileDB::makeForClangCL("test.cpp");
+
+        {
+            std::shared_ptr<TestConfigImpl> config = std::make_shared<TestConfigImpl>();
+            config->settings_.useSystemStdlib = false;
+            config->settings_.stdlibIncludes.push_back("stdlib-path");
+
+            auto adjusted = adjustCompileCommand(db, config);
+            BOOST_TEST(has(adjusted, "/X"));
+            BOOST_TEST(has(adjusted, {"-external:I", "stdlib-path"}));
+        }
+
+        {
+            std::shared_ptr<TestConfigImpl> config = std::make_shared<TestConfigImpl>();
+            config->settings_.useSystemLibc = false;
+            config->settings_.libcIncludes.push_back("libc-path");
+
+            auto adjusted = adjustCompileCommand(db, config);
+            BOOST_TEST(has(adjusted, "-nostdinc"));
+            BOOST_TEST(has(adjusted, {"-external:I", "libc-path"}));
+        }
+    }
+
+    void run()
+    {
+        testClang();
+        testClangCL();
+    }
+};
+
+TEST_SUITE(
+    MrDocsCompilationDatabase_test,
+    "clang.mrdocs.MrDocsCompilationDatabase");
+
+} // mrdocs
+} // clang
