b5cdfa3d5f448b40fb9a8f2316d40adbe7bbd888
Matheus Izvekov <mizvekov@gmail.com>
2025-10-06 17:17:31 -0300

    don't mark unnamed classes as implementation defined
    These classes can appear as user written code and needs to be
    documented, as show in the new test case.
    
    In general, for implementation defined declarations, we can detect them
    as the clang AST exposes them as 'implicit', but this is possible
    for any declaration, and can be handled more generally.
    
    The new included exposes a different bug, where we don't attach the
    documentation provided for each member in a declarator, which clang
    does expose in the AST, but we seem to be ignoring it.
    
    Fixes #1035

--- test-files/golden-tests/symbols/record/unnamed.cpp
+++ test-files/golden-tests/symbols/record/unnamed.cpp
@@ -2,3 +2,13 @@ constexpr struct
 {
     void operator()() const;
 } const F{};
+
+/// A test unnamed class.
+extern struct {
+  /// A test field.
+  int a;
+}
+  /// A test variable named 'x'
+  x,
+  /// A test variable named 'y'
+  y;
