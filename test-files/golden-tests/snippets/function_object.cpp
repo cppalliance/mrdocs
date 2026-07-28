87bca18e5d8c87a55b86c0e43353ef206d3dfa8a
Alan de Freitas <alandefreitas@gmail.com>
2026-07-27 21:06:01 -0500

    docs: landing snippets as headers

--- test-files/golden-tests/snippets/function_object.cpp
+++ test-files/golden-tests/snippets/function_object.cpp
@@ -1,13 +1,1 @@
-struct abs_fn
-{
-    /** Compute the absolute value.
-
-        @param x The input value.
-        @return The absolute value of x.
-    */
-    double
-    operator()(double x) const noexcept;
-};
-
-/** Return the absolute value of a number. */
-constexpr abs_fn abs = {};
+#include "function_object.hpp"
