7cff518054be7d39aab8291cd1051e45f4874a39
Alan de Freitas <alandefreitas@gmail.com>
2026-07-27 20:49:49 -0500

    docs: sqrt snippet is declaration only

--- test-files/golden-tests/snippets/sqrt.cpp
+++ test-files/golden-tests/snippets/sqrt.cpp
@@ -12,22 +12,4 @@
     @return The square root of the input value.
  */
 template <typename T>
-std::enable_if_t<std::is_integral_v<T>, T> sqrt(T value) {
-    if (value < 0) {
-        throw std::invalid_argument(
-            "Cannot compute square root of a negative number");
-    }
-    T result = 0;
-    // The second-to-top bit is set
-    T bit = 1 << (sizeof(T) * 8 - 2);
-    while (bit > value) bit >>= 2;
-    while (bit != 0) {
-        if (value >= result + bit) {
-            value -= result + bit;
-            result += bit << 1;
-        }
-        result >>= 1;
-        bit >>= 2;
-    }
-    return result;
-}
+std::enable_if_t<std::is_integral_v<T>, T> sqrt(T value);
